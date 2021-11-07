888236ea99e624c996eae6e5eaacdbb6eaf9c5f1
EC2 Default User <ec2-user@ip-172-31-79-219.ec2.internal>
2021-11-07 01:56:35 +0000

    UPDATE index画面

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,9 +4,23 @@ class BooksController < ApplicationController
     @books = Book.all
   end
 
+  def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+  end
+
   def show
   end
 
   def edit
+  end
+
+  private
+  def book_params
+    params.permit(:title, :body)
   end
 end
