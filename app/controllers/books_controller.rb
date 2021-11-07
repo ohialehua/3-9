1a5e4242c2900a4306cea353e2da8d9a5cf79fed
EC2 Default User <ec2-user@ip-172-31-79-219.ec2.internal>
2021-11-07 17:23:54 +0000

    ADD flashメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,6 +12,9 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     if @book.save
       redirect_to book_path(@book.id)
+      flash[:notice] = 'Book was successfully created.'
+    else
+      render :index
     end
   end
 
@@ -25,18 +28,26 @@ class BooksController < ApplicationController
 
   def destroy
     book = Book.find(params[:id])
-    book.destroy
-    redirect_to books_path
+    if book.destroy
+      redirect_to books_path
+      flash[:notice] = 'Book was successfully destroyed.'
+    else
+      render :index
+    end
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+      flash[:notice] = 'Book was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   private
   def book_params
-    params.require(:book).permit(:title, :body)
+    params.permit(:title, :body)
   end
 end
